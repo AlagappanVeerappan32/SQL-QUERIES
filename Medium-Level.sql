4ae83b97975382d3a59eec5e06d1f0c5ad80b617
AlagappanVeerappan <133504573+AlagappanVeerappan32@users.noreply.github.com>
2024-09-26 14:26:48 -0300

    Update Medium-Level.sql

--- Medium-Level.sql
+++ Medium-Level.sql
@@ -1,1 +1,11 @@
+1. Workers With The Highest Salaries.You have been asked to find the job titles of the highest-paid employees.
+Your output should include the highest-paid title or multiple titles with the same salary?
+
+SELECT T.worker_title FROM worker W JOIN title T
+ON T.worker_ref_id = W.worker_id
+WHERE W.salary = (SELECT MAX(salary) FROM worker)
+ORDER BY W.salary desc
+
+
+
 
