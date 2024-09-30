2b31b99e14dd0aa157a7e6f62fdba275b688d242
AlagappanVeerappan <133504573+AlagappanVeerappan32@users.noreply.github.com>
2024-09-30 09:46:52 -0300

    Update Hard-Level.sql

--- Hard-Level.sql
+++ Hard-Level.sql
@@ -15,3 +15,16 @@ SELECT search_id,
     END )AS rating
 FROM fb_search_events
 GROUP BY search_id
+
+2. ABC Corp is a mid-sized insurer in the US and in the recent past their fraudulent claims have increased significantly for their personal auto insurance portfolio. They have developed a ML based predictive model to identify propensity of fraudulent claims. Now, they assign highly experienced claim adjusters for top 5 percentile of claims identified by the model.
+Your objective is to identify the top 5 percentile of claims from each state. Your output should be policy number, state, claim cost, and fraud score?
+
+WITH ranked_claims AS (
+    SELECT policy_num, state, claim_cost, fraud_score,
+        NTILE(100) OVER (PARTITION BY state ORDER BY fraud_score DESC) AS percentile_rank
+    FROM fraud_score
+)
+SELECT policy_num, state, claim_cost, fraud_score
+FROM ranked_claims
+WHERE percentile_rank <= 5;
+
