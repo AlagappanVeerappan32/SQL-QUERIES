d9c44e62564d0baeaeb66595d7dba1404df8af36
AlagappanVeerappan <133504573+AlagappanVeerappan32@users.noreply.github.com>
2024-10-12 13:31:53 -0300

    Update AdventureWorks.sql

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -131,3 +131,135 @@ GROUP BY OrganizationLevel;
 SELECT BusinessEntityID, FLOOR(TIMESTAMPDIFF(YEAR, BirthDate, '2014-08-05')) AS age
 FROM HumanResources_Employee
 ORDER BY 2 DESC
+
+-- Question 16:
+
+-- a. How many products are sold by AdventureWorks?
+SELECT COUNT(ProductID) FROM Production_Product
+WHERE FinishedGoodsFlag=1;
+
+-- b. How many of these products are actively being sold by AdventureWorks?
+# FinishedGoodsFlag = 0 - not for sale, 1 - for sale.
+
+SELECT COUNT(ProductID) FROM Production_Product
+WHERE FinishedGoodsFlag=1 and SellEndDate is null;
+
+-- c. How many of these active products are made in house vs. purchased?
+# MAKE FLAG = 0-PRODUCT IS PURCHASED, 1 = PRODUCT IS MADE IN HOUSE.
+    
+SELECT COUNT(ProductID),
+COUNT(CASE 
+WHEN MakeFlag = 0 THEN ProductID
+END) as Purchased,
+COUNT(CASE 
+WHEN MakeFlag = 1 THEN ProductID
+END) as made_product
+FROM Production_Product
+WHERE FinishedGoodsFlag=1 and SellEndDate is null;
+
+
+-- Question 17
+
+-- We learned in Question 16 that the product table includes a few different type of products - i.e., manufactured vs. purchased.
+
+-- a. Sum the LineTotal in SalesOrderDetail. Format as currency
+
+SELECT CONCAT('$', FORMAT(SUM(LineTotal), 2)) AS lineTotal FROM Sales_SalesOrderDetail;
+
+-- b. Sum the LineTotal in SalesOrderDetail by the MakeFlag in the product table. Use a case statement to specify manufactured vs. purchased. Format as currency.
+
+SELECT 
+CASE WHEN P.MakeFlag = 1 THEN 'manufactured' ELSE 'purchased' END AS MakeFlag,
+CONCAT('$', FORMAT(SUM(S.LineTotal), 2)) AS lineTotal
+FROM Sales_SalesOrderDetail S 
+JOIN Production_Product P ON P.ProductID = S.ProductID
+GROUP BY p.MakeFlag;
+
+
+-- c. Add a count of distinct SalesOrderIDs
+SELECT 
+CASE WHEN P.MakeFlag = 1 THEN 'manufactured' ELSE 'purchased' END AS MakeFlag,
+CONCAT('$', FORMAT(SUM(S.LineTotal), 2)) AS lineTotal,
+COUNT(DISTINCT S.SalesOrderID) as salesId
+FROM Sales_SalesOrderDetail S 
+JOIN Production_Product P ON P.ProductID = S.ProductID
+GROUP BY p.MakeFlag;
+
+-- d. What is the average LineTotal per SalesOrderID?
+
+SELECT 
+CASE WHEN P.MakeFlag = 1 THEN 'manufactured' ELSE 'purchased' END AS MakeFlag,
+CONCAT('$', FORMAT(SUM(S.LineTotal), 2)) AS lineTotal,
+COUNT(DISTINCT S.SalesOrderID) as salesId,
+FORMAT(SUM(S.LineTotal)/COUNT(DISTINCT S.SalesOrderID),2) as avg_total
+FROM Sales_SalesOrderDetail S 
+JOIN Production_Product P ON P.ProductID = S.ProductID
+GROUP BY p.MakeFlag;
+
+
+-- Question 18
+
+-- The AdventureWorks Cyclery database includes historical and present transactions.
+
+-- a. In the TransactionHistory and TransactionHistoryArchive tables a "W","S", and "P" are used as Transaction types.    What do these abbreviations mean?
+# w- workorder, s-salesorder, p-purchaseorder
+
+-- b. Union TransactionHistory and TransactionHistoryArchive
+
+SELECT * FROM Production_TransactionHistory
+UNION
+SELECT * FROM Production_TransactionHistoryArchive;
+
+-- c. Find the First and Last TransactionDate in the TransactionHistory and TransactionHistoryArchive tables. Use the union written in part b. The current data type for TransactionDate is datetime. Convert or Cast the data type to date.
+
+SELECT
+max(DATE(TransactionDate)) as last_date,
+min(DATE(TransactionDate)) as first_date
+FROM (SELECT * FROM Production_TransactionHistory
+UNION
+SELECT * FROM Production_TransactionHistoryArchive) AS innerQ;
+
+-- d. Find the First and Last Date for each transaction type. Use a case statement to specify the transaction types. 
+
+SELECT 
+CASE WHEN TransactionType = 'W' THEN 'workorder'
+WHEN TransactionType = 'S' THEN 'salesorder'
+ELSE 'purchaseorder' END as TransactionType,
+max(DATE(TransactionDate)) as last_date,
+min(DATE(TransactionDate)) as first_date
+FROM (SELECT * FROM Production_TransactionHistory
+UNION
+SELECT * FROM Production_TransactionHistoryArchive) AS innerQ
+group by TransactionType;
+
+-- Question 19
+
+-- We learned in Question 18 that the most recent SalesOrder transaction occurred on 2014-06-30 and the First Sales Order transaction occurred on 2011-05-31. 
+-- Does the SalesOrderHeader table show a similar Order date for the first and Last Sale? Format as Date 
+
+SELECT
+max(DATE(OrderDate)) as last_date,
+min(DATE(OrderDate)) as first_date
+FROM 
+Sales_SalesOrderHeader;
+
+
+-- Question 20
+
+-- We learned in Question 19 that the first and most recent OrderDate in the SalesOrderHeader t
+-- able matches the Sales Order Dates in the transactionHistory table (Question 18).
+
+-- a. Find the other tables and dates that should match the WorkOrder and PurchaseOrder Dates. Format these dates as a date in the YYYY-MM-DD format.
+
+SELECT 'workorder',
+max(DATE(StartDate)) as last_date,
+min(DATE(StartDate)) as first_date
+FROM 
+Production_WorkOrder
+UNION
+SELECT 'purchaseorder',
+max(DATE(OrderDate)) as last_date,
+min(DATE(OrderDate)) as first_date
+FROM 
+Purchasing_PurchaseOrderHeader
+where status = 1;
